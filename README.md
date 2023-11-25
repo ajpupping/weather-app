9864cf58fbbc6deb6cba999e681ef25563508998
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-11-25 11:53:22 -0800

    added function to fetch geo code, added link to deployed site in readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weather-app
+
+[Check the Weather](https://ajpupping.github.io/weather-app/)
--- script.js
+++ script.js
@@ -1,20 +1,34 @@
 var APIKey = '8cc7cf2e740792b898cecd92170389f2';
 var city = document.getElementById('city-input');
+var state = state;
+var country = country;
 
 // for testing
 city = 'san diego'
 
-var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+var weatherQueryURL = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
+var geoQueryURL = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=' + 5 + '&appid=' + APIKey;
+
+function getGeoCode() {
+    fetch(geoQueryURL)
+        .then(function (response) { return response.json() })
+        .then(function (data) {
+            // for testing    
+            console.log(data);
+        });
+}
 
 
 function getWeather() {
-    fetch(queryURL)  
-    .then(function(response) { return response.json() })
-    .then(function(data) {
-    // for testing    
-    console.log(data);
-    });
+    fetch(weatherQueryURL)
+        .then(function (response) { return response.json() })
+        .then(function (data) {
+            // for testing    
+            console.log(data);
+        });
 }
 
 getWeather();
+
+getGeoCode()
 
