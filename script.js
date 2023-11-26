4730aeb818ca814e7545cfcfe9a8d35633eda6ca
Alyssa <125580811+ajpupping@users.noreply.github.com>
2023-11-25 20:28:41 -0800

    added city list function, used geocode to update weather query url, added functionality to search input

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
 var APIKey = '8cc7cf2e740792b898cecd92170389f2';
-var city = document.getElementById('city-input');
-var state = state;
-var country = country;
+var searchForm = document.getElementById('search-form');
+var latitude;
+var longitude;
+var cityValue;
+var cityList;
+var cityListEl = document.getElementById('city-list')
 
-// for testing
-city = 'san diego'
+function handleSearch(event) {
+    event.preventDefault();
+    cityValue = document.getElementById('city-input').value
 
-var weatherQueryURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey;
-var geoQueryURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=' + 5 + '&appid=' + APIKey;
+    console.log(cityValue);
 
-function getGeoCode() {
+    getGeoCode(cityValue);
+    createList(cityValue)
+}
+
+function createList(city) {
+    var cityLi = document.createElement('li');
+    cityLi.textContent = city;
+    cityListEl.appendChild(cityLi)
+
+}
+
+searchForm.addEventListener('submit', handleSearch);
+
+function getGeoCode(city) {
+    var geoQueryURL = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=' + 1 + '&appid=' + APIKey;
+
     fetch(geoQueryURL)
         .then(function (response) { return response.json() })
         .then(function (data) {
-            // for testing    
             console.log(data);
+            latitude = data[0].lat;
+            longitude = data[0].lon;
+            console.log(latitude)
+            console.log(longitude)
+
+            updateWeatherQuery(latitude, longitude)
         });
 }
 
-
-function getWeather() {
-    fetch(weatherQueryURL)
-        .then(function (response) { return response.json() })
-        .then(function (data) {
-            // for testing    
-            console.log(data);
-        });
+function updateWeatherQuery(latitude, longitude) {
+    var weatherQueryURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + APIKey;
+    console.log(weatherQueryURL)
 }
 
-getWeather();
+//function getWeather() {
+  //  fetch(weatherQueryURL)
+    //    .then(function (response) { return response.json() })
+      //  .then(function (data) {
+         //   console.log(data); // for testing  
+        //});
+//}
 
-getGeoCode()
-
+//getWeather();
